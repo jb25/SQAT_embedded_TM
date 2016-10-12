4a657df7d8bc167ce23678208d911c014e00b924
student <student@stupid-pc0184.stnet.local>
2016-10-12 11:23:19 +0300

    b

--- src/TemperatureMonitoring.cpp
+++ src/TemperatureMonitoring.cpp
@@ -30,6 +30,43 @@ int main(void)
 #if defined (__USE_LPCOPEN)
     SystemCoreClockUpdate();
 #endif
+    swm_config_i2c();
+    i2c_reset();
+	disp_on( DISP_SHOW_NONE );
+	disp_reset( DISP_SHOW_NONE );
 
+	tm_reset_data();
+	/**
+	 * TODO: Oscar to remove this
+	 */
+
+	int i;
+	for( i=0; i<9; i++){
+		tm_handle_sensor();
+	}
+	display_message_t trend = tm_get_trend();
+	disp_show_message( trend );
+	/**
+	 * TODO: Oscar to remove until here
+	 */
+
+
+	while( 1 ){
+		/**
+		 * TODO: Oscar to remove this
+		 */
+
+
+
+
+
+
+		tm_handle_sensor();
+		trend = tm_get_trend();
+		disp_show_message( trend );
+		/**
+		 * TODO: Oscar to remove until here
+		 */
+	}
     return 0 ;
 }
